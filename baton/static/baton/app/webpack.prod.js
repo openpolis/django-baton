8f2a7dcbdefa8327f57fff14a7abab0ec4822a87
guglielmo <guglielmo@openpolis.it>
2021-11-02 09:13:35 +0100

    pnrr s3 url added to prod config for webpack

--- baton/static/baton/app/webpack.prod.js
+++ baton/static/baton/app/webpack.prod.js
@@ -2,6 +2,10 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: false,
+  mode: 'production',
+  output: {
+    ...common.output,
+    publicPath: "https://openpnrr.s3.eu-central-1.amazonaws.com/static/baton/app/dist/",
+  },
+  devtool: false,
 })
